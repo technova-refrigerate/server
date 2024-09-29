9e387d9dc9e26d0172d91e8a6035610eb1296bf1
Hazel G <58535328+toaster203@users.noreply.github.com>
2024-09-29 02:20:46 -0400

    add user inventory endpoints

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: false,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+//   quantity: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//   },
+  bestBefore: {
+    type: Date,
+    required: true,
+  },
+  dateStored: {
+    type: Date,
+    default: Date.now,
+  },
+  storageLocation: {
+    type: String,
+    enum: ["Fridge", "Freezer"],
+    required: true,
+  },
+});
+
+const UserSchema = new mongoose.Schema({
+  propelUserID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  products: [ProductSchema],
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
--- server.js
+++ server.js
@@ -1,38 +1,101 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './connectDB.js';
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./connectDB.js";
+dotenv.config();
 
-import Product from './models/productModel.js';
+import Product from "./models/productModel.js";
+import { initAuth } from "@propelauth/express";
+import User from "./models/userModel.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
 connectDB();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello!');
-})
-
-app.get('/products/all', async (req, res) => {
-    try {
-        const products = await Product.find({}, "_id Name Name_subtitle");
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.get('/products/:id', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-        res.json(product);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+const {
+  requireUser,
+  fetchUserMetadataByUserId,
+  // ...
+} = initAuth({
+  authUrl: process.env.PROPEL_AUTH_URL,
+  apiKey: process.env.PROPEL_API_KEY,
+});
+app.get("/api/whoami", requireUser, (req, res) => {
+  res.send("Hello user with ID " + req.userClass.userId);
 });
 
-app.listen(port, ()=> console.log(`Server running on port ${port}`));
+app.get("/api/products/all", requireUser, async (req, res) => {
+  try {
+    const products = await Product.find({}, "_id Name Name_subtitle");
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", requireUser, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products", requireUser, async (req, res) => {
+  try {
+    let user = await User.findOne({ propelAccessToken: req.user.accessToken });
+
+    if (user) {
+      res.json(user.products);
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/api/products", requireUser, async (req, res) => {
+  console.log(req.body);
+  const { id, icon, name, subtitle, bestBefore, storageLocation } = req.body;
+
+  try {
+    let user = await User.findOne({ propelUserID: req.user.userId });
+    console.log("user", user);
+    if (!user) {
+      console.log("making new user");
+      console.log("user", req.user);
+      user = new User({
+        propelUserID: req.user.userId,
+        products: [],
+      });
+      console.log("new user", user);
+    }
+
+    const newProduct = {
+      id,
+      icon,
+      name,
+      subtitle,
+      bestBefore,
+      storageLocation,
+    };
+
+    user.products.push(newProduct);
+    console.log("user after product", user);
+
+    await user.save();
+    console.log("user after save", user);
+
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+app.listen(port, () => console.log(`Server running on port ${port}`));
